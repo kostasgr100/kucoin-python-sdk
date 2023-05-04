29761ad1a3f50afb074b6ce676cc309ef0369ea8
kostasgr100 <97601678+kostasgr100@users.noreply.github.com>
2023-05-04 22:39:00 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup
 
 
 setup(
-    name='kucoin-python',
+    name='my_kucoin',
     version='v1.0.11',
-    packages=['kucoin', 'kucoin/base_request', 'kucoin/margin', 'kucoin/market', 'kucoin/trade', 'kucoin/user',
-              'kucoin/websocket', 'kucoin/ws_token'],
+    packages=['my_kucoin', 'my_kucoin/base_request', 'my_kucoin/margin', 'my_kucoin/market', 'my_kucoin/trade', 'my_kucoin/user',
+              'my_kucoin/websocket', 'my_kucoin/ws_token'],
     license="MIT",
     author='Arthur',
     author_email="arthur.zhang@kucoin.com",
-    url='https://github.com/Kucoin/kucoin-python-sdk',
+    url='https://github.com/kostasgr100/kucoin-python-sdk/',
     description="kucoin-api-sdk",
     install_requires=['requests', 'websockets'],
     classifiers=[
